d1f7e77266c388adc3dcb9ee81c545e4ee790e67
Marek Kaput <marek.kaput@outlook.com>
2017-09-12 17:45:38 +0200

    rename

--- boot/x86_64/multiboot.asm
+++ boot/x86_64/multiboot.asm
@@ -4,7 +4,7 @@
 ; http://git.savannah.gnu.org/cgit/grub.git/tree/doc/multiboot.texi?h=multiboot2
 ; http://git.savannah.gnu.org/cgit/grub.git/tree/doc/multiboot2.h?h=multiboot2
 
-section .multiboot_header
+section .multiboot
 header_start:
     dd 0xe85250d6                ; magic number (multiboot 2)
     dd 0                         ; architecture 0 (protected mode i386)

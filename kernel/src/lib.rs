7585c5cdc92faccc4ec1156baa905337cc0bbda4
Marek Kaput <marek.kaput@outlook.com>
2017-09-17 22:11:11 +0200

    upd

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -13,14 +13,14 @@ pub extern "C" fn krnl_main() {
 
     let mut hello_colored = [color_byte; 24];
     for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
+        hello_colored[i * 2] = *char_byte;
     }
 
     // write `Hello World!` to the center of the VGA text buffer
     let buffer_ptr = (0xb8000 + 1988) as *mut _;
     unsafe { *buffer_ptr = hello_colored };
 
-    loop{}
+    loop {}
 }
 
 #[lang = "eh_personality"]
